4b0549126b056ac18beeffc919fb40448c0378ad
Parthasarathy <zeromoddev@gmail.com>
2017-10-22 13:29:54 +0530

    Update config_local.sh

--- config_local.sh
+++ config_local.sh
@@ -1,3 +1,3 @@
 curl https://dl.google.com/dl/cloudsdk/release/install_google_cloud_sdk.bash | bash
 gcloud auth login
-gcloud compute ssh cosmic-zeromod --project ubuntu-167608 --zone us-central1-f --ssh-flag "-L 5901:localhost:5901"
+gcloud compute ssh zeromod --project sacred-brace-173107 --zone asia-southeast1-b --ssh-flag "-L 5901:localhost:5901"
