10a49152a994110145694efaa4b145293a06b0ba
Parthasarathy <zeromoddev@gmail.com>
2017-10-22 12:54:07 +0530

    Update guiforgc.sh

--- guiforgc.sh
+++ guiforgc.sh
@@ -1,19 +1,33 @@
+#pre_checks
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install gnome-shell
-sudo apt-get install ubuntu-gnome-desktop
-sudo apt-get install autocutsel
-sudo apt-get install gnome-core
-sudo apt-get install gnome-panel
-sudo apt-get install tightvncserver
+
+#install necessary packages
+sudo apt-get -y install gnome-shell
+sudo apt-get -y install ubuntu-gnome-desktop
+sudo apt-get -y install autocutsel
+sudo apt-get -y install gnome-core
+sudo apt-get -y install gnome-panel
+sudo apt-get -y install tightvncserver
+
+#fix Xresource bug in tightvncserver
 touch ~/.Xresources
+
+#start the vncserver
 vncserver
+
+#fix xstartup
 rm ~/.vnc/xstartup
-cp ~/guiforgc/xstartup ~/.vnc/
-sudo apt-get install gnome-themes-standard
-sudo apt-get install adwaita-icon-theme-full adwaita-icon-theme
+cp xstartup ~/.vnc/
+
+#install themes(optional)
+sudo apt-get -y install gnome-themes-standard
+sudo apt-get -y install adwaita-icon-theme-full adwaita-icon-theme
+
+#config themes
 gsettings get org.gnome.metacity theme
 gsettings set org.gnome.metacity theme 'Adwaita'
+
+#vncserver geometry (resolution)
 vncserver -geometry 1024x640
 vncserver -kill :1
-
